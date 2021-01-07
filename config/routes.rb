62a449cb696df1dce2763d3457442ba2cf33515d
Romesh Thavanathan <romeshthavanathan@gmail.com>
2021-01-07 17:14:00 +0000

    add index route to admin:categories resource

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, only: [:create, :new]
+    resources :categories, only: [:index, :create, :new]
     
   end
 
