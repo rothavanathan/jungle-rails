68c6b4c86abcbe4156ba3e347c590e7ed4ada2a0
Romesh Thavanathan <romeshthavanathan@gmail.com>
2021-01-11 23:40:44 +0000

    added attr_accesor to user model for new first_name and last_name columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  # validates :email, uniqueness: true 
   validates :email, presence: true
+
+  attr_accessor :first_name, :last_name
 end
