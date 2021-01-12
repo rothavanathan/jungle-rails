616b571d6e9fb3cf4b2c0cc3fd8809a4abf5f780
Romesh Thavanathan <romeshthavanathan@gmail.com>
2021-01-12 00:06:02 +0000

    specs for users model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  
   validates :first_name, presence: true
   validates :last_name, presence: true
-  # validates :email, uniqueness: true 
+  validates :email, uniqueness: true 
   validates :email, presence: true
-
+  validates :password, length: { minimum: 5 }
+  
   attr_accessor :first_name, :last_name
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,11 +7,32 @@ RSpec.describe User, type: :model do
   end
 
 
+
   it 'will not save when passwords do not match' do
     @new_user = User.new(first_name: "Popeye", last_name: "Jones", email: "popeye@gmail.com",password: "password123", password_confirmation: "watermelon123")
     @new_user.save
     expect(@new_user.errors.messages).to include(:password_confirmation=>["doesn't match Password"])
   end
 
+  it 'will save when email is unique' do
+    @new_user = User.new(first_name: "Popeye", last_name: "Jones", email: "popeye@gmail.com", password: "password123", password_confirmation: "password123")
+    @new_user.save
+    @new_user2 = User.new(first_name: "Jurgen", last_name: "Potter", email: "hooray@gmail.com", password: "password123", password_confirmation: "password123")
+    @new_user2.save
+  end
+
+  it 'will not save when email is not unique' do
+    @new_user = User.new(first_name: "Popeye", last_name: "Jones", email: "popeye@gmail.com", password: "password123", password_confirmation: "password123")
+    @new_user.save
+    @new_user2 = User.new(first_name: "Jurgen", last_name: "Potter", email: "popeye@gmail.com", password: "password123", password_confirmation: "password123")
+    @new_user2.save
+    expect(@new_user2.errors.messages).to include(:email=>["has already been taken"])
+  end
+
+  it 'will not save when password is less than 5 characters long' do
+    @new_user = User.new(first_name: "Popeye", last_name: "Jones", email: "popeye@gmail.com", password: "pass", password_confirmation: "pass")
+    @new_user.save
+    expect(@new_user.errors.messages).to include(:password=>["is too short (minimum is 5 characters)"])
+  end
 
 end
